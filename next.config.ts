1f834fce4ca12ac325bd26a6fb5638b242d188e8
BhaktiDhawan333 <bhaktidhawan2810@gmail.com>
2025-04-25 17:10:28 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 // /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true, 
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+// next.config.js
+//  @type {import('next').NextConfig} 
 const nextConfig = {
   experimental: {
-    appDir: true, 
+    appDir: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
+
